08d84a15555f8be44334188a78c1020906bb908b
francine265 <fmatho@yahoo.com>
2022-11-23 04:32:12 -0500

    creation de la classe Trajets

---
+++ ProjetFinal/ProjetFinal/GestionBD.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetFinal
+{
+    internal class GestionBD
+    {
+    }
+}
