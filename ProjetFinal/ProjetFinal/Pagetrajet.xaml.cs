9c5e2a7aa5eb797d68ab0edf4811115e37cc823f
francine265 <fmatho@yahoo.com>
2022-12-12 18:24:07 -0500

    ajout

--- ProjetFinal/ProjetFinal/Pagetrajet.xaml.cs
+++ ProjetFinal/ProjetFinal/Pagetrajet.xaml.cs
@@ -7,6 +7,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -27,5 +28,81 @@ namespace ProjetFinal
         {
             this.InitializeComponent();
         }
+        private void trajet_Click(object sender, RoutedEventArgs e)
+        {
+
+            DateTime d1 = new DateTime();
+            DateTime d2 = new DateTime();
+            int valide = 0;
+
+
+            try
+            {
+                /// d1 = calendar.Date.Value.Date;
+                /// 
+                d1 = calender1.Date.Value.DateTime;
+
+
+            }
+            catch (InvalidOperationException ex)
+            {
+                erreurcalender1.Visibility = Visibility.Visible;
+                valide += 1;
+            }
+
+            try
+            {
+                /// d1 = calendar.Date.Value.Date;
+                /// 
+
+                d2 = caleder2.Date.Value.DateTime;
+
+            }
+            catch (InvalidOperationException ex)
+            {
+                erreurcalender2.Visibility = Visibility.Visible;
+                valide += 1;
+            }
+
+
+
+            if (valide == 0)
+            {
+                lv.ItemsSource = GestionBD.getInstance().GetTrajetsdate(d1, d2);
+            }
+
+            //d2 = caleder2.Date.Value.DateTime;
+
+
+
+        }
+
+        private async void ecritureCSV_Click(object sender, RoutedEventArgs e)
+        {
+
+            var picker = new Windows.Storage.Pickers.FileSavePicker();
+
+            /******************** POUR WINUI3 ***************************/
+
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(GestionBD.getInstance().Fenetre);
+            WinRT.Interop.InitializeWithWindow.Initialize(picker, hWnd);
+            /************************************************************/
+
+            picker.SuggestedFileName = "Liste de Trajets";
+            picker.FileTypeChoices.Add("Fichier CSV", new List<string>() { ".csv" });
+
+            //crée le fichier
+            Windows.Storage.StorageFile monFichier = await picker.PickSaveFileAsync();
+
+            var liste = lv.ItemsSource as ObservableCollection<Trajets>;
+            var l = liste.ToList();
+            //var liste = Singleton.getInstance().GetTrajetsdate()
+
+            //écrit dans le fichier chacune des lignes du tableau
+            //une boucle sera faite sur la collection et prendra chacun des objets de celle-ci
+            await Windows.Storage.FileIO.WriteLinesAsync(monFichier, l.ConvertAll(x => x.exportationCSV()), Windows.Storage.Streams.UnicodeEncoding.Utf8);
+
+        }
     }
+
 }
